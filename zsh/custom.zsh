ecc96fe06e3e642d8097bdb517530fdcbed35eb5
Casey Lang <clang1234@gmail.com>
2014-06-30 15:17:56 -0500

    Add way too many zsh plugins

---
+++ zsh/custom.zsh
@@ -0,0 +1,31 @@
+# skips dupes when using up/down arrows to look through your history
+setopt HIST_FIND_NO_DUPS
+
+export EDITOR=vim
+
+# human readable sizes
+alias df="df -H"
+
+# human readable sizes and a total
+alias du="du -hc"
+
+# create sub directories all at once
+alias mkdir="mkdir -p"
+
+# list with human readable sizes
+alias ll='ls -oh'
+
+# list only dot files/directories
+alias l.='ll -Ad \.*'
+
+# ignore case when searching
+alias less='less -i'
+
+# man is supposed to default to this but case insensitive search isn't working
+alias man='man -P "less -is"'
+
+# make directory and change
+mdc() {
+  mkdir $1
+  cd $1
+}
